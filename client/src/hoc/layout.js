41933e97220e2f2eb9470f45b585f6e571822eb6
Marcos Scorzoni <mlessa@gmail.com>
2018-11-11 11:39:13 +0000

    add header

--- client/src/hoc/layout.js
+++ client/src/hoc/layout.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Header from '../components/Header_Footer/Header';
+
 class Layout extends Component {
   render() {
     return (
       <div>
-        Header
+        <Header />
         <div className="page_container">
           {this.props.children}
         </div>
