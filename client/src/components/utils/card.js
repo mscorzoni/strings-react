3be6586149c8c0bcd75251b148eeab5f948a1696
Marcos Scorzoni <mlessa@gmail.com>
2018-11-13 21:52:55 +0000

    add cards

---
+++ client/src/components/utils/card.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+
+  renderCardImage(images){
+    if(images.length > 0){
+      return images[0].url
+    }else{
+      return '/images/guitars/image_not_available.png'
+    }
+  }
+
+  render() {
+    const props = this.props;
+
+    return (
+      <div className={`card_item_wrapper ${props.grip}`}>
+        <div
+          className="image"
+          style={{
+            background: `url(${this.renderCardImage(props.images)}) no-repeat`
+          }}
+        >
+        </div>
+        <div className="action_container">
+          <div className="tags">
+            <div className="brand">{props.brand.name}</div>
+            <div className="name">{props.name}</div>
+            <div className="name">${props.price}</div>
+          </div>
+        </div>
+        { props.grid ?
+          <div className="description">
+            Description
+          </div>
+          : null
+        }
+      </div>
+    );
+  }
+}
+
+export default Card;
